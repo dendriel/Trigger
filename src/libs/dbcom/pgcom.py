955498625be61c4880e3ba3db2eb5a4d4b34fbb7
vitor.r2 <vitor.rozsa@hotmail.com>
2012-03-06 22:32:02 -0300

    editing pgcom

--- src/libs/dbcom/pgcom.py
+++ src/libs/dbcom/pgcom.py
@@ -1,11 +1,11 @@
 # -*-coding:utf-8-*-
 from defines import *
 import psycopg2
+import psycopg2.extensions
 
 class Pgcom:
 
-
-    def __init__(self, db_host, db_user, db_pass, db_name, log_path):
+    def __init__(self, db_host, db_user, db_pass, db_name, log_obj):
         """
         Brief: Initialize the database conection parameters.
         Param: db_host The ip where are the database.
@@ -18,23 +18,41 @@ class Pgcom:
         self.db_user = db_user
         self.db_pass = db_pass
         self.db_name = db_name
-      #  self.log = slog(log_path)
-        self.com = ''
+        self.db_port = 5433
+        self.log = log_obj
+        self.conn = ''
         self.cursor = ''
 
     def start(self):
 
         try:
-            conn = psycopg2.connect("\
-            dbname='trigger'\
-            user='trigger'\
-            host='127.0.1.1'\
-            password='trigger'\
-            ");
-            self.cur = conn.cursor();
-            
-        except:
-            print "Erro ao se conectar a base de dados!";
+            self.conn = psycopg2.connect("\
+            dbname=%s\
+            user=%s\
+            host=%s\
+            port= %d\
+            password=%s\
+            " % (self.db_name, self.db_user, self.db_host, self.db_port, self.db_pass))
+            self.cursor = self.conn.cursor()
+            self.__query("lol")
+            return OK
+
+        except Exception, exc:
+            #self.log.LOG(LOG_CRITICAL, "Pgcom.start()", "Erro ao se conectar a base de dados!");
+            print"%s: %s" % (exc.__class__.__name__, exc)
+            return ERROR
+
+    def __query(self, query):
+
+        try:
+            self.cursor.execute("select *  from teste")
+            print self.cursor.fetchall()
+            return OK
+        except Exception, exc:
+            #self.log.LOG(LOG_CRITICAL, "Pgcom.start()", "Erro ao se conectar a base de dados!");
+            print"%s: %s" % (exc.__class__.__name__, exc)
+            return ERROR
+
 
     #método destrutor
     def __del__(self):
@@ -46,6 +64,6 @@ class Pgcom:
 #---------------------------------------------------------#
 if __name__ == "__main__":
 
-    dbcom = Pgcom(DB_HOST, DB_USER, DB_PASS, DB_NAME, SYSTEM_LOG_PATH)
+    dbcom = Pgcom("localhost", "trigger", "trigger", "trigger", SYSTEM_LOG_PATH)
     dbcom.start()
 #---------------------------------------------------------#
